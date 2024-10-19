72907e5475da203e5bf17e8db22493cc128bcfda
AngelPalaciosN <palaciosangeldavidn@gmail.com>
2024-10-18 19:49:15 -0500

    Recreacion Sec1
    Se creo la sec1 apartir de un concepto

--- proXimidad/src/components/modules/Header.jsx
+++ proXimidad/src/components/modules/Header.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import '../../scss/component-styles/Header.scss'
+import { Container, Row, Col } from 'react-bootstrap';
+import '../../scss/component-styles/Header.scss';
 
-function Header (){
-    return(
-        <>
-            <div className='container-fluid' id='head'>
-                <h1>proXimidad</h1>
-            </div>
-        </>
-    )
-}
-
+const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <Row className="align-items-center">
+          <Col>
+            <h1 className="text-white">ProXimidad</h1>
+          </Col>
+          <Col>
+            <nav>
+              <ul className="list-unstyled d-flex justify-content-end mb-0">
+                <li><a href="#" className="text-white me-3">Inicio</a></li>
+                <li><a href="#" className="text-white me-3">Servicios</a></li>
+                <li><a href="#" className="text-white me-3">Acerca de</a></li>
+                <li><a href="#" className="text-white">Contacto</a></li>
+              </ul>
+            </nav>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
+};
 
 export default Header;
--- proXimidad/src/components/modules/Sec1.jsx
+++ proXimidad/src/components/modules/Sec1.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import Header from './Header';
+import '../../scss/component-styles/Sec1.scss';
 
-function Sec1 (){
-    return (
-        <>
-        <section id='sec1' className="py-5">
-            <div className='container'>
-                <p>Arrienda a cardenas a 1 cop</p>
-            </div>
+export default function Sec1() {
+  return (
+    <div className="sec1">
+        <section className="hero">
+          <Container>
+            <Row className="align-items-center">
+              <Col md={6}>
+                <h2 className="text-primary mb-4">Bienvenido a Nuestra Página</h2>
+                <p className="text-secondary mb-4">Ofrecemos a cardenas por 100 pesos</p>
+                <Button variant="primary" size="lg">Comienza Ahora</Button>
+              </Col>
+              <Col md={6}>
+                <div className="contact-form-container">
+                  <h3 className="text-primary mb-4">Contáctanos</h3>
+                  <Form>
+                    <Form.Group className="mb-3" controlId="formName">
+                      <Form.Control type="text" placeholder="Nombre" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formEmail">
+                      <Form.Control type="email" placeholder="Correo electrónico" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formMessage">
+                      <Form.Control as="textarea" rows={3} placeholder="Mensaje" />
+                    </Form.Group>
+                    <Button variant="secondary" type="submit" className="w-100">
+                      Enviar
+                    </Button>
+                  </Form>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </section>
-        </>
-    )
+       </div> 
+  );
 }
-
-
-export default Sec1;
